44447f59f9d3aeb6281e2e3ea1af9e1bf42f8e0c
Dmitry Goncharov <dgoncharov@users.sf.net>
2009-07-06 13:33:53 +0000

    added an all-embracing try-catch block in main()

--- remv/remv.cpp
+++ remv/remv.cpp
@@ -90,84 +90,97 @@ std::ostream& operator<<(std::ostream& out, options const& opts)
 
 int main(int argc, char* argv[])
 {
-    namespace po = boost::program_options;
-    string const exe = fs::path(argv[0]).leaf();
-    options opts;
+    try
     {
-        po::options_description desc(
-            "Usage: " + exe + " regex format [path] [options]"
-            "\n"
-            "\nStarting from #path recursively finds files and directories"
-	    "\nwhich match the regular expression, specified by #regex,"
-            "\nand moves them according to the format, specified by #format."
+        namespace po = boost::program_options;
+        string const exe = fs::path(argv[0]).leaf();
+        options opts;
+        {
+            po::options_description desc(
+                "Usage: " + exe + " regex format [path] [options]"
+                "\n"
+                "\nStarting from #path recursively finds files and directories"
+    	    "\nwhich match the regular expression, specified by #regex,"
+                "\nand moves them according to the format, specified by #format."
+        
+                "\n\nThe Boost.Regex library is used for regex processing."
+                "\n(See boost.org for details on supported regex syntaxes)."
+                "\n\nIf path is not specified . is used."
     
-            "\n\nThe Boost.Regex library is used for regex processing."
-            "\n(See boost.org for details on supported regex syntaxes)."
-            "\n\nIf path is not specified . is used."
-
-            "\n\nExamples: Moving all uppercase chars to lowercase chars: " + exe  + " \"(\\w+)\" \"\\L\\\\1\""
-            "\n\nOptions");
+                "\n\nExamples: Moving all uppercase chars to lowercase chars: " + exe  + " \"(\\w+)\" \"\\L\\\\1\""
+                "\n\nOptions");
+        
+            desc.add_options()
+                ("help,h",
+                    "this help message")
+                ("version,v",
+                    "print the version")
+                ("recursive,r",
+                    "move directories recursively")
+                ("pretend,p",
+                    "don't actually move")
+                ("force,f",
+                    "overwrite existing files");
     
-        desc.add_options()
-            ("help,h",
-                "this help message")
-            ("version,v",
-                "print the version")
-            ("recursive,r",
-                "move directories recursively")
-            ("pretend,p",
-                "don't actually move")
-            ("force,f",
-                "overwrite existing files");
-
-        po::positional_options_description pos;
-        pos.add("regex", 1);
-        pos.add("format", 1);
-        pos.add("path", 1);
-
-        po::options_description hidden("Hidden options");
-        hidden.add_options()
-            ("regex", po::value<string>(&opts.regex))
-            ("format", po::value<string>(&opts.format))
-            ("path", po::value<fs::path>(&opts.path)->default_value(fs::path(".")));
-
-        po::options_description all_opts("All options");
-        all_opts.add(desc);
-        all_opts.add(hidden);
-
-        po::variables_map vm;
-        po::store(po::command_line_parser(argc, argv).options(all_opts).positional(pos).run(), vm);
-        notify(vm);
-        opts.recursive = vm.count("recursive"); //TODO: have the lib to init it.
-        opts.pretend = vm.count("pretend"); //TODO: have the lib to init it.
-        opts.force = vm.count("force"); //TODO: have the lib to init it.
-
-        if (vm.count("help"))
-        {
-            cout << desc << endl;
-            return 0;
+            po::positional_options_description pos;
+            pos.add("regex", 1);
+            pos.add("format", 1);
+            pos.add("path", 1);
+    
+            po::options_description hidden("Hidden options");
+            hidden.add_options()
+                ("regex", po::value<string>(&opts.regex))
+                ("format", po::value<string>(&opts.format))
+                ("path", po::value<fs::path>(&opts.path)->default_value(fs::path(".")));
+    
+            po::options_description all_opts("All options");
+            all_opts.add(desc);
+            all_opts.add(hidden);
+    
+            po::variables_map vm;
+            po::store(po::command_line_parser(argc, argv).options(all_opts).positional(pos).run(), vm);
+            notify(vm);
+            opts.recursive = vm.count("recursive"); //TODO: have the lib to init it.
+            opts.pretend = vm.count("pretend"); //TODO: have the lib to init it.
+            opts.force = vm.count("force"); //TODO: have the lib to init it.
+    
+            if (vm.count("help"))
+            {
+                cout << desc << endl;
+                return 0;
+            }
+            if (vm.count("version"))
+            {
+                cout << VERSION << endl;
+                return 0;
+            }
+            if (!vm.count("regex")) //TODO: have the lib to report this
+            {
+                std::cerr << fs::path(argv[0]).leaf() << ": missing regex operand" << endl;
+                return 1;
+            }
+            if (!vm.count("format"))
+            {
+                std::cerr << fs::path(argv[0]).leaf() << ": missing format operand" << endl;
+                return 2;
+            }
+            cout << opts << endl;
         }
-        if (vm.count("version"))
-        {
-            cout << VERSION << endl;
-            return 0;
-        }
-        if (!vm.count("regex")) //TODO: have the lib to report this
-        {
-            std::cerr << fs::path(argv[0]).leaf() << ": missing regex operand" << endl;
-            return 1;
-        }
-        if (!vm.count("format"))
-        {
-            std::cerr << fs::path(argv[0]).leaf() << ": missing format operand" << endl;
-            return 2;
-        }
-        cout << opts << endl;
+    
+        boost::regex const r(opts.regex);
+        moveall(opts.path, r, opts.format, opts.recursive, opts.pretend, opts.force);
     }
-
-    boost::regex const r(opts.regex);
-    moveall(opts.path, r, opts.format, opts.recursive, opts.pretend, opts.force);
-
+    catch (std::exception const& ex)
+    {
+        cerr << ex.what() << endl;
+        return 3;
+    }
+    catch (...)
+    {
+        cerr << "unknown exception" << endl;
+        return 4;
+    }
     return 0;
+
 }
 
